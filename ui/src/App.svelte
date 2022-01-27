85db972d87b67d010b6df7d0b716819cda551d4b
Bart6114 <bart@dataroots.io>
2022-01-27 21:58:02 +0100

    svelte ui skeleton

---
+++ ui/src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let name;
+</script>
+
+<main>
+  <h1 class="bg-blue-600">phonehome.dev</h1>
+  <p>KISS telemetry for FOSS packages.</p>
+  <div class="mb-6">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="username"
+        type="text"
+        placeholder="organisation/repository"
+      />
+    </div>
+  </div>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
